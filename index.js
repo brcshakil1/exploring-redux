ce3cdfa5690ca157cc4132cd20b2da8edbecad9c
brcshakil1 <brcshakil72@gmail.com>
2024-01-15 14:34:30 +0600

    redux - state, action and reducer

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//  DEFINING CONSTANTS
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+
 // state
 const initialCounterState = {
   count: 0,
@@ -6,6 +11,48 @@ const initialUsersState = {
   users: [{ name: "Bayazid" }],
 };
 
-// action
-// INCREMENT COUNTER
-// DECREMENT COUNTER
+// action - object: type, payload
+
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const addUser = () => {
+  return {
+    type: ADD_USER,
+    payload: { name: "Shakil" },
+  };
+};
+
+// create reducer for counter
+
+const decrementCounter = (state = initialCounterState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1
+      }
+  
+    default:
+      state;
+  }
+}
+
+// 1. state
+// 2. dispatch action
+// 3. reducer (will work based on action type)
+// 4. store
